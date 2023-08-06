79c3638be8ab0847beaee3479a40364ac97ee18a
judemuego <55373216+sciontech-inc@users.noreply.github.com>
2023-08-06 17:26:02 +0800

    prescription template

--- resources/views/backend/partial/component/prescription.blade.php
+++ resources/views/backend/partial/component/prescription.blade.php
@@ -10,12 +10,17 @@
 <style>
     body {
         font-family: 'Poppins';
+        color: black;
+        background-image: url("/images/medical-bg.png");
+        background-position: center center;
+        background-size: contain;
+        background-repeat: no-repeat;
     }
 </style>
 <body>
     <div class="row">
         <div class="col-12">
-            <div class="print_prescription" style="width: 8.5in; height: 11in; margin:auto; border: 1px Solid; padding: 15px;">
+            <div class="print_prescription" style="width: 8.5in; margin:auto; border: 1px Solid; padding: 15px;">
                 <div class="row">
                     <div class="col-12">
                         <p style="margin: 0px; font-size: 24px; color: #086f9e; font-weight: bold;"><span>Dr. Anandibai G. Joshi</span></p>
@@ -27,8 +32,7 @@
                     </div>
                     <div class="spacer" style="padding: 1em;"></div>
                 </div>
-                <div class="row">
-                   
+                <div class="row" style="margin-bottom: 10px;">
                     <div class="col-6" style="font-size: 14px; display: inline-flex;">
                         <div style="margin: 0px 10px 0px 0px;">
                             <i class="fas fa-phone"></i>
@@ -41,7 +45,140 @@
                     </div>
                     <div class="col-6">
                         
-                        </div>
+                    </div>
+                </div>
+                <div class="row" style="border-top: 1px dashed; padding-top: 10px;">
+                    <div class="col-6">
+                        <span style="font-weight: bold; color: black;">Juan Dela Cruz</span>
+                    </div>
+                    <div class="col-3" style="text-align: center;">
+                        <span style="font-weight: bold; color: black;">60 YEARS OLD</span>
+                    </div>
+                    <div class="col-3" style="text-align: center;">
+                        <span style="font-weight: bold; color: black;">MALE</span>
+                    </div>
+                </div>
+                <div style="padding: 1em;"></div>
+                <div class="row">
+                    <div class="col-12">
+                        <span style="font-weight: bold; color: black;">Complaints:</span> Cough with fever x 5 days
+                    </div>
+                </div>
+                <div class="row" style="padding-top: 10px;">
+                    <div class="col-12">
+                        <span style="font-weight: bold; color: black;">Vitals</span>
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >Pulse Rate: </span>90 bpm
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >Pulse Pattern: </span>Regular
+                    </div>
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >BP: </span> 146/88 mmHg
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >Resp Rate: </span> 22 cycles/min
+                    </div>
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >Rest Pattern: </span> Regular
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >Temp: </span> 38 Celsius
+                    </div>
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >SpO2: </span> 90%
+                    </div>
+                    <div class="col-4" style="font-size: 14px;">
+                        <span >RBS: </span> 110%
+                    </div>
+                    <div class="col-4">
+                    </div>
+                </div>
+                <div style="padding: 0.5em;"></div>
+                <div class="row">
+                    <div class="col-12">
+                        <span style="font-weight: bold; color: black;">Examination:</span> B/L Ronchi + 
+                    </div>
+                </div>
+                <div style="padding: 5px"></div>
+                <div class="row">
+                    <div class="col-12">
+                        <span style="font-weight: bold; color: black; background: #ffd600; padding: 3px;">Diagnosis:</span> Fever for Evaluation
+                    </div>
+                </div>
+                <div style="padding: 0.5em"></div>
+                <div class="row">
+                    <div class="col-12">
+                        <img style="width:80px;" src="/images/rx-icon.png" alt="">
+                    </div>
+                </div>
+                <div style="padding: 0.5em"></div>
+                <div class="row" style="margin-top: 5px;">
+                    <div class="col-12">
+                        <p style="margin: 0px; font-size: 16px; color: #086f9e; font-weight: bold; margin-left: 50px;"><span>1. Augmentin 625 Duo Tablet</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; font-weight: bold; margin-left: 50px; font-style: italic;"><span>Amoxycillin (500mg) + Clavulanic Acid (125mg)</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>1 Tablet - 0 - 1 tablet for 5 days</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>Instructions: Take on empty stomach</span></p>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                    <div class="col-12">
+                        <p style="margin: 0px; font-size: 16px; color: #086f9e; font-weight: bold; margin-left: 50px;"><span>2. Crocin Advance Tablet</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; font-weight: bold; margin-left: 50px; font-style: italic;"><span>Paracetamol (500mg)</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>1 Tablet when required 5 days</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>Instructions: After meal</span></p>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                    <div class="col-12">
+                        <p style="margin: 0px; font-size: 16px; color: #086f9e; font-weight: bold; margin-left: 50px;"><span>3. Zivinix C Chewable Tablet</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>Components: Complete Blood Counts, Liver Function Tests, Renal Function Tests, Urine routine & microscopy</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>Instructions: Empty stomach sample is not necessary</span></p>
+                    </div>
+                </div>
+                <div style="padding: 1em;"></div>
+                <div class="row">
+                    <div class="col-12">
+                        <span style="font-weight: bold; color: black;">Investigations</span>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                    <div class="col-12">
+                        <p style="margin: 0px; font-size: 16px; color: #086f9e; font-weight: bold; margin-left: 50px;"><span>1. CxR P/A View</span></p>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                    <div class="col-12">
+                        <p style="margin: 0px; font-size: 16px; color: #086f9e; font-weight: bold; margin-left: 50px;"><span>2. CBC, LFT, RFT, Urine routine & microscopy</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; font-weight: bold; margin-left: 50px; font-style: italic;"><span>Vitamin C (400mg) + Zinc Sulfate (7.5mg)</span></p>
+                        <p style="margin: 0px; color: #535353; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>1 Tablet - 0 - 1 tablet for 14 days</span></p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <p style="margin: 0px; font-size: 16px; color: #086f9e; font-weight: bold; margin-left: 50px;"><span>3. COVID-19 RT PCR</span></p>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                    <div class="col-12">
+                        <span style="font-weight: bold; color: black;">Advice:</span> Review with reports
+                    </div>
+                </div>
+                <div style="padding: 1em;"></div>
+                <div class="row">
+                    <div class="col-6">
+                    </div>
+                    <div class="col-6" style="text-align: center;">
+                        <img style="width:80px;" src="/images/sample-signature.png" alt="">
+                        <p style="margin: 0px; color: #535353; font-weight: bold; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>Dr. Anandibai G. Joshi</span></p>
+                        <p style="margin: 0px; color: #535353; font-weight: bold; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>MBBS, M.D Medicine</span></p>
+                        <p style="margin: 0px; color: #535353; font-weight: bold; font-size: 14px; margin-top: -5px; margin-left: 50px;"><span>MCI 110008</span></p>
+                    </div>
                 </div>
             </div>
         </div>

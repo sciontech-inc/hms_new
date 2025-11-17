29c0bf5478d1f158667a9e90f8541707ad5888ac
judemuego <47876710+judemuego@users.noreply.github.com>
2025-11-17 19:42:40 +0800

    mediq logo

--- resources/views/backend/master/index.blade.php
+++ resources/views/backend/master/index.blade.php
@@ -8,7 +8,7 @@
     <title>Business Solutions</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    
+    <link rel="icon" type="image/x-icon" href="/images/logo.png">
     <script src="{{asset('/js/jquery.validate.min.js')}}" ></script>
     <script src="{{asset('/plugins/moment.js')}}" ></script>
     <link href="{{{ URL::asset('backend/css/modern.css') }}}" rel="stylesheet">
